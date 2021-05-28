979d9e46b94f7e29e1e6d2521447ad175d5fe35d
harrypotter2021 <newalexey2020@gmail.com>
2021-05-28 21:18:25 +0900

    seller page is started

---
+++ src/components/banner/BannerFourSingle.js
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const BannerFourSingle = ({ data, spaceBottomClass }) => {
+  return (
+    <div className="col-lg-4 col-md-4">
+      <div
+        className={`single-banner banner-shape banner-green-color ${
+          spaceBottomClass ? spaceBottomClass : ""
+        }`}
+      >
+        <Link to={process.env.PUBLIC_URL + data.link}>
+          <img src={process.env.PUBLIC_URL + data.image} alt="" />
+        </Link>
+        <div className="banner-content" style={{ marginRight:"2vw",marginLeft:"1vw"  }}>
+          <h4 style={{ fontFamily:"sans-serif" }}>{data.title}</h4>
+          <h5 style={{paddingTop:"-30px", fontFamily:"sans-serif" }}>
+            {data.subtitle} <span>{data.price}</span>
+          </h5>
+          {/* <Link to={process.env.PUBLIC_URL + data.link}>
+            <i className="fa fa-long-arrow-right" />
+          </Link> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+BannerFourSingle.propTypes = {
+  data: PropTypes.object,
+  spaceBottomClass: PropTypes.string
+};
+
+export default BannerFourSingle;
